4e02b9824d9b0fd6a54a896626d7e2936e182fd2
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2025-01-13 01:41:46 +0900

    slkdflsdf

--- src/screens/postCardDetail/[id].tsx
+++ src/screens/postCardDetail/[id].tsx
@@ -8,13 +8,22 @@ export default function PostCardDetail() {
 
   return (
     <View style={styles.container}>
+      <Text style={styles.team}>인사팀</Text>
       <Text style={styles.title}>게시글 상세 페이지</Text>
       <Text style={styles.content}>게시글 ID: {id}</Text>
+      <Text style={styles.like}>300</Text>
+      <Text style={styles.eyes}>40</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  team : {
+    backgroundColor: "#575555",
+    color : "#fff",
+    fontSize : 10,
+    borderRadius: 20
+  },
   container: {
     flex: 1,
     justifyContent: "center",
@@ -30,4 +39,12 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: "#555",
   },
+  like : {
+    fontSize: 10,
+    color: "#575555"
+  },
+  eyes : {
+    fontSize: 10,
+    color: "#575555"
+  }
 });
