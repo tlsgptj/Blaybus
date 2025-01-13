9956fb28600603b3135853593cd99f036344eec4
DongHyun <pca468@naver.com>
2025-01-14 01:01:41 +0900

    donghyun commit

--- src/screens/CalendarScreen.tsx
+++ src/screens/CalendarScreen.tsx
@@ -9,7 +9,7 @@ import {
   View,
 } from "react-native";
 
-function CalendarScreen() {
+function CalendarScreen({ navigation }) {
   const [currentDate, setCurrentDate] = useState(new Date());
   const [isMonthSelectorVisible, setMonthSelectorVisible] = useState(false);
 
@@ -18,12 +18,30 @@ function CalendarScreen() {
   const [selectedTasks, setSelectedTasks] = useState([]);
 
   const [isAddTaskModalVisible, setIsAddTaskModalVisible] = useState(false);
-  const [newTask, setNewTask] = useState({title: '', description : ''})
+  const [newTask, setNewTask] = useState({ title: "", description: "" });
 
   const tasks = [
-    { id: '1', date: '1월 6일', title: '팀퀘스트', description: 'Lorem ipsum dolor sit amet.', tag: 'very good' },
-    { id: '2', date: '1월 6일', title: '리더퀘스트', description: 'Lorem ipsum dolor sit amet.', tag: 'good' },
-    { id: '3', date: '1월 6일', title: '활일', description: 'Lorem ipsum dolor sit amet.', tag: 'neutral' },
+    {
+      id: "1",
+      date: "1월 6일",
+      title: "팀퀘스트",
+      description: "Lorem ipsum dolor sit amet.",
+      tag: "very good",
+    },
+    {
+      id: "2",
+      date: "1월 6일",
+      title: "리더퀘스트",
+      description: "Lorem ipsum dolor sit amet.",
+      tag: "good",
+    },
+    {
+      id: "3",
+      date: "1월 6일",
+      title: "활일",
+      description: "Lorem ipsum dolor sit amet.",
+      tag: "neutral",
+    },
   ];
 
   const daysInMonth = (year, month) => new Date(year, month + 1, 0).getDate();
@@ -61,9 +79,11 @@ function CalendarScreen() {
 
   const handleDayPress = (day) => {
     if (day) {
-      const dateString = `${currentDate.getFullYear()}-${(currentDate.getMonth() + 1)
+      const dateString = `${currentDate.getFullYear()}-${(
+        currentDate.getMonth() + 1
+      )
         .toString()
-        .padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+        .padStart(2, "0")}-${day.toString().padStart(2, "0")}`;
       const filteredTasks = tasks.filter((task) => task.date === dateString);
       setSelectedDate(dateString);
       setSelectedTasks(filteredTasks);
@@ -73,7 +93,7 @@ function CalendarScreen() {
 
   const handleAddTask = () => {
     setIsAddTaskModalVisible(false);
-  }
+  };
 
   const handleMonthSelect = (monthIndex) => {
     const newDate = new Date(currentDate);
@@ -111,8 +131,17 @@ function CalendarScreen() {
 
   return (
     <View style={styles.container}>
-      {/* Header */}
+      {/* header */}
       <View style={styles.header}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Text>뒤로로</Text>
+        </TouchableOpacity>
+        <Text style={styles.headerTitle}>퀘스트</Text>
+        <TouchableOpacity><Text>알람</Text></TouchableOpacity>
+      </View>
+
+      {/* calendar Month */}
+      <View style={styles.headerMonthSelector}>
         <TouchableOpacity
           style={styles.monthSelectorButton}
           onPress={() => setMonthSelectorVisible(true)}
@@ -163,7 +192,7 @@ function CalendarScreen() {
       {/* Calendar */}
       <View style={styles.calendarContainer}>
         <View style={styles.dayLabels}>
-          {['일', '월', '화', '수', '목', '금', '토'].map((day, index) => (
+          {["일", "월", "화", "수", "목", "금", "토"].map((day, index) => (
             <Text key={index} style={styles.dayLabel}>
               {day}
             </Text>
@@ -173,8 +202,14 @@ function CalendarScreen() {
         {calendar.map((week, index) => (
           <View key={index} style={styles.week}>
             {week.map((day, dayIndex) => (
-              <TouchableOpacity key={dayIndex} style={styles.day} onPress={() => handleDayPress(day)}>
-                <Text style={[styles.dayText, day === null && styles.emptyDay]}>{day || ''}</Text>
+              <TouchableOpacity
+                key={dayIndex}
+                style={styles.day}
+                onPress={() => handleDayPress(day)}
+              >
+                <Text style={[styles.dayText, day === null && styles.emptyDay]}>
+                  {day || ""}
+                </Text>
               </TouchableOpacity>
             ))}
           </View>
@@ -235,7 +270,9 @@ function CalendarScreen() {
               placeholder="할 일 설명"
               multiline
               value={newTask.description}
-              onChangeText={(text) => setNewTask({ ...newTask, description: text })}
+              onChangeText={(text) =>
+                setNewTask({ ...newTask, description: text })
+              }
             />
 
             <TouchableOpacity
@@ -260,6 +297,7 @@ function CalendarScreen() {
         위 스케줄은 고객님이 삭제하기 전까지 1년간 보관되며, 1년 뒤 내용 및
         스케줄은 자동 삭제 처리됩니다.
       </Text>
+      
     </View>
   );
 }
@@ -272,9 +310,17 @@ const styles = StyleSheet.create({
   },
   header: {
     flexDirection: "row",
-    justifyContent: "center",
+    justifyContent:"space-between",
     alignItems: "center",
     marginBottom: 20,
+  },
+  headerTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  headerMonthSelector: {
+    alignItems: "center",
+    marginVertical: 10,
   },
   monthSelectorButton: {
     paddingVertical: 5,
@@ -305,24 +351,24 @@ const styles = StyleSheet.create({
   },
   modalTitle: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
   },
   noTaskText: {
     fontSize: 16,
-    color: '#888',
-    textAlign: 'center',
+    color: "#888",
+    textAlign: "center",
   },
   closeButton: {
     marginTop: 20,
     padding: 10,
-    backgroundColor: '#ddd',
+    backgroundColor: "#ddd",
     borderRadius: 5,
-    alignSelf: 'center',
+    alignSelf: "center",
   },
   closeButtonText: {
     fontSize: 16,
-    color: '#555',
+    color: "#555",
   },
   monthOption: {
     paddingVertical: 15,
@@ -388,14 +434,14 @@ const styles = StyleSheet.create({
   taskItem: {
     padding: 10,
     marginBottom: 10,
-    backgroundColor: '#f9f9f9',
+    backgroundColor: "#f9f9f9",
     borderRadius: 5,
     borderWidth: 1,
-    borderColor: '#ddd',
+    borderColor: "#ddd",
   },
   taskHeader: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginBottom: 5,
   },
   taskTag: {
@@ -403,28 +449,28 @@ const styles = StyleSheet.create({
     paddingHorizontal: 8,
     paddingVertical: 4,
     borderRadius: 5,
-    color: '#fff',
+    color: "#fff",
   },
   verygood: {
-    backgroundColor: '#ff4d4d',
+    backgroundColor: "#ff4d4d",
   },
   good: {
-    backgroundColor: '#ffcd3c',
+    backgroundColor: "#ffcd3c",
   },
   neutral: {
-    backgroundColor: '#d3d3d3',
+    backgroundColor: "#d3d3d3",
   },
   taskTitle: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   taskDescription: {
     fontSize: 14,
-    color: '#666',
+    color: "#666",
   },
   input: {
     height: 40,
-    borderColor: '#ddd',
+    borderColor: "#ddd",
     borderWidth: 1,
     marginBottom: 15,
     borderRadius: 5,
@@ -432,39 +478,39 @@ const styles = StyleSheet.create({
   },
   textArea: {
     height: 100,
-    borderColor: '#ddd',
+    borderColor: "#ddd",
     borderWidth: 1,
     marginBottom: 15,
     borderRadius: 5,
     paddingHorizontal: 10,
-    textAlignVertical: 'top',
+    textAlignVertical: "top",
   },
   submitButton: {
-    backgroundColor: '#ff6347',
+    backgroundColor: "#ff6347",
     padding: 10,
     borderRadius: 5,
-    alignItems: 'center',
+    alignItems: "center",
   },
   submitButtonText: {
-    color: '#fff',
-    fontWeight: 'bold',
+    color: "#fff",
+    fontWeight: "bold",
     fontSize: 16,
   },
   addButtonInModal: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 20,
     right: 20,
     width: 50,
     height: 50,
     borderRadius: 25,
-    backgroundColor: '#ff6347',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#ff6347",
+    justifyContent: "center",
+    alignItems: "center",
   },
   addButtonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
 
