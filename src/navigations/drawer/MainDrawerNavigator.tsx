e36da977099e42e8fadb458e8248706f95c71b17
DongHyun <pca468@naver.com>
2025-01-14 17:31:19 +0900

    edit ui success

--- src/navigations/drawer/MainDrawerNavigator.tsx
+++ src/navigations/drawer/MainDrawerNavigator.tsx
@@ -4,11 +4,9 @@ import { StyleSheet, View } from "react-native";
 import Main from "../../screens/Main";
 import MyPage from "../../screens/MyPage";
 import BoardMain from "../../screens/BoardMain";
-import Post from "../../screens/Post";
 import AllExperience from "../../screens/AllExperience";
 import EditProfile from "../../screens/EditProfile";
 import LandingPage from "../../screens/Landing";
-import { NavigationContainer } from "@react-navigation/native";
 import CalendarScreen from "../../screens/CalendarScreen";
 import AlarmScreen from "../../screens/AlarmScreen";
 import PushAlramScreen from "../../screens/PushAlram";
@@ -20,12 +18,11 @@ function MainDrawerNavigator() {
     <Drawer.Navigator>
       <Drawer.Screen name="main" component={Main} />
       <Drawer.Screen name="mypage" component={MyPage} />
-      <Drawer.Screen name="board" component={BoardMain} />
-      <Drawer.Screen name="post" component={Post} />
-      <Drawer.Screen name="experience" component={AllExperience} />
+      <Drawer.Screen name="board" component={BoardMain} options={{drawerItemStyle: {display: "none"}}}/>
+      <Drawer.Screen name="experience" component={AllExperience} options={{drawerItemStyle: {display: "none"}}}/>
       <Drawer.Screen name="edit" component={EditProfile} />
       <Drawer.Screen name="landing" component={LandingPage} />
-      <Drawer.Screen name="calendar" component={CalendarScreen} />
+      <Drawer.Screen name="calendar" component={CalendarScreen} options={{drawerItemStyle: {display: "none"}}}/>
       <Drawer.Screen name="alarm" component={AlarmScreen} />
       <Drawer.Screen name="PushAlram" component={PushAlramScreen}/>
     </Drawer.Navigator>
--- src/screens/EditProfile.tsx
+++ src/screens/EditProfile.tsx
@@ -1,45 +1,74 @@
-import React, { useState } from 'react';
-import {Image, StyleSheet, Switch, Text, TextInput, TouchableOpacity, View} from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-
-
+import React, { useState } from "react";
+import {
+  Image,
+  StyleSheet,
+  Switch,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 function EditProfile() {
-    const [currentPassword, setCurrentPassword] = useState('');
-    const [newPassword, setNewPassword] = useState('');
-    const [isNotification, setIsNotification] = useState(false);
-    const [selectedCharacter, setSelectedCharacter] = useState(null);
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [isNotification, setIsNotification] = useState(false);
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
 
-    const characters = [
-        { id: 1, uri: 'https://via.placeholder.com/50' },
-        { id: 2, uri: 'https://via.placeholder.com/50' },
-        { id: 3, uri: 'https://via.placeholder.com/50' },
-        { id: 4, uri: 'https://via.placeholder.com/50' },
-        { id: 5, uri: 'https://via.placeholder.com/50' },
-        { id: 6, uri: 'https://via.placeholder.com/50' },
-      ];
+  const characters = [
+    { id: 1, uri: require("../assets/images/mypage/man-01.png") },
+    { id: 2, uri: require("../assets/images/mypage/man-02.png") },
+    { id: 3, uri: require("../assets/images/mypage/man-03.png") },
+    { id: 4, uri: require("../assets/images/mypage/man-05.png") },
+    { id: 5, uri: require("../assets/images/mypage/woman-01.png") },
+    { id: 6, uri: require("../assets/images/mypage/woman-04.png") },
+    { id: 7, uri: require("../assets/images/mypage/woman-03.png") },
+    { id: 8, uri: require("../assets/images/mypage/woman-05.png") },
+  ];
 
-    const toggleNotification = () => {
-        setIsNotification((prev) => !prev);
-    }
+  const toggleNotification = () => {
+    setIsNotification((prev) => !prev);
+  };
   return (
     <ScrollView style={styles.container}>
-        <View style={styles.section}>
-        <Text style={styles.sectionTitle}>비밀번호 변경</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="현재 비밀번호를 입력해주세요."
-          secureTextEntry
-          value={currentPassword}
-          onChangeText={setCurrentPassword}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="변경할 비밀번호를 입력해주세요."
-          secureTextEntry
-          value={newPassword}
-          onChangeText={setNewPassword}
-        />
+      <View style={styles.section}>
+        <View style={styles.titleContainer}>
+          <Image
+            source={require("../assets/images/mypage/User_perspective_matte.png")}
+            style={styles.icon}
+          />
+          <Text style={styles.sectionTitle}>비밀번호 변경</Text>
+        </View>
+        <View style={styles.separator}/>
+        <View style={styles.inputContainer}>
+          <Image
+            source={require("../assets/images/mypage/lock.png")}
+            style={{width: 20, height: 20}}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="현재 비밀번호를 입력해주세요."
+            secureTextEntry
+            value={currentPassword}
+            onChangeText={setCurrentPassword}
+          />
+        </View>
+       
+        <View style={styles.inputContainer}>
+          <Image
+            source={require("../assets/images/mypage/lock.png")}
+            style={{ width: 20, height: 20 }}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="변경할 비밀번호를 입력해주세요."
+            secureTextEntry
+            value={newPassword}
+            onChangeText={setNewPassword}
+          />
+        </View>
+
         <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.cancelButton}>
             <Text style={styles.buttonText}>취소</Text>
@@ -52,7 +81,14 @@ function EditProfile() {
 
       {/* Character Change Section */}
       <View style={styles.section}>
-        <Text style={styles.sectionTitle}>캐릭터 변경</Text>
+      <View style={styles.titleContainer}>
+          <Image
+            source={require("../assets/images/mypage/User_perspective_matte.png")}
+            style={styles.icon}
+          />
+          <Text style={styles.sectionTitle}>캐릭터 변경</Text>
+        </View>
+        <View style={styles.separator}/>
         <View style={styles.characterContainer}>
           {characters.map((character) => (
             <TouchableOpacity
@@ -63,7 +99,7 @@ function EditProfile() {
               ]}
               onPress={() => setSelectedCharacter(character.id)}
             >
-              <Image style={styles.characterImage} source={{ uri: character.uri }} />
+              <Image style={styles.characterImage} source={character.uri} />
             </TouchableOpacity>
           ))}
         </View>
@@ -83,95 +119,125 @@ function EditProfile() {
         <Switch
           value={isNotification}
           onValueChange={toggleNotification}
-          thumbColor={isNotification ? '#ff5722' : '#f4f3f4'}
-          trackColor={{ false: '#767577', true: '#ffccbc' }}
+          thumbColor={isNotification ? "#ff5722" : "#f4f3f4"}
+          trackColor={{ false: "#767577", true: "#ffccbc" }}
         />
       </View>
-
     </ScrollView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        padding: 20,
-      },
-      section: {
-        backgroundColor: '#f8f8f8',
-        borderRadius: 10,
-        padding: 20,
-        marginBottom: 20,
-      },
-      sectionTitle: {
-        fontSize: 16,
-        fontWeight: 'bold',
-        marginBottom: 10,
-      },
-      input: {
-        borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 5,
-        padding: 10,
-        marginBottom: 10,
-        backgroundColor: '#fff',
-      },
-      buttonContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-      },
-      cancelButton: {
-        backgroundColor: '#e0e0e0',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderRadius: 5,
-      },
-      submitButton: {
-        backgroundColor: '#ff5722',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderRadius: 5,
-      },
-      buttonText: {
-        color: '#fff',
-        fontWeight: 'bold',
-        textAlign: 'center',
-      },
-      characterContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginVertical: 10,
-      },
-      character: {
-        width: 50,
-        height: 50,
-        borderRadius: 25,
-        borderWidth: 2,
-        borderColor: '#ccc',
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-      selectedCharacter: {
-        borderColor: '#ff5722',
-      },
-      characterImage: {
-        width: 40,
-        height: 40,
-        borderRadius: 20,
-      },
-      notificationSection: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: 20,
-        backgroundColor: '#f8f8f8',
-        borderRadius: 10,
-      },
-      notificationText: {
-        fontSize: 16,
-        fontWeight: 'bold',
-      },
+  container: {
+    flex: 1,
+    backgroundColor: "#f8f8f8",
+    padding: 20,
+  },
+  section: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 20,
+    marginBottom: 20,
+  },
+  icon: {
+    width: 24,
+    height: 24,
+    marginRight: 8,
+    borderRadius: 12, // 아이콘 둥글게 설정
+  },
+  titleContainer: {
+    display: "flex",
+    flexDirection: "row",
+    gap: "0.3rem",
+  },
+  sectionTitle: {
+    fontSize: 16,
+    fontWeight: "500",
+    marginBottom: 10,
+  },
+  separator: {
+    marginTop: 8,
+    height: 1,
+    backgroundColor: "#eee", 
+    marginBottom: 8
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#f8f8f8",
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    marginBottom: 15,
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#ccc",
+  },
+  input: {
+    flex: 1,
+    fontSize: 12,
+    color: "#ccc",
+    marginLeft: 5,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  cancelButton: {
+    backgroundColor: "#e0e0e0",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  submitButton: {
+    backgroundColor: "#ff5722",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  characterContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    marginVertical: 10,
+    gap: "0.3rem",
+  },
+  character: {
+    width: "22%",
+    height: 55,
+    aspectRatio: 1,
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: "#ccc",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  selectedCharacter: {
+    borderColor: "#ff5722",
+  },
+  characterImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 20,
+  },
+  notificationSection: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+  },
+  notificationText: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
 });
 
 export default EditProfile;
